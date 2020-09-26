ad6420b4f3596e3f3ad566e1112f221fbbc4dd73
churpita <32397472+churpita@users.noreply.github.com>
2020-09-26 17:52:27 -0400

    added collision, fixed setTexture on GridSquare class
    +fixed setTexture to push changes to GridSquare textures
    +added get/set methods for collision option on GridSquare
    +started work on click functions within GridSquare
    
    known bugs:
    -java version problems with lamda functions when enabling click functions in LevelScreen

--- core/src/com/kacstudios/game/GridSquare.java
+++ core/src/com/kacstudios/game/GridSquare.java
@@ -1,25 +1,40 @@
 package com.kacstudios.game;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class GridSquare extends BaseActor {
 
-    public GridSquare(float x, float y, Stage s)
+    private boolean collideWithPlayer;
+
+    public GridSquare(float x, float y, Stage s, boolean collides)
     {
         super(x,y,s);
+        collideWithPlayer = collides;
         loadTexture("grid_blank.png");
-//        loadTexture("grid_red.png");
     }
 
     public void setTexture(String path) {
-        System.out.println("set");
         try {
-            loadTexture("grid_red.png");
+            Animation<TextureRegion> loadNewTexture = loadTexture(path);
+            setAnimation(loadNewTexture);
         }
         catch (Exception ie) {
             System.out.println(ie);
         }
+    }
 
+    public void setCollisionSetting(boolean collides) {
+        collideWithPlayer = collides;
+    }
+
+    public boolean getCollisionSetting() {
+        return collideWithPlayer;
+    }
+
+    public void clickFunction() {
+        System.out.println("I was clicked!");
     }
 
 }
--- core/src/com/kacstudios/game/LevelScreen.java
+++ core/src/com/kacstudios/game/LevelScreen.java
@@ -2,6 +2,8 @@ package com.kacstudios.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,10 @@ import java.util.List;
 public class LevelScreen extends BaseScreen {
     private Farmer farmer;
     private List<GridSquare> gridSquares;
+
+
+
+
 
     public void initialize() {
 
@@ -22,16 +28,63 @@ public class LevelScreen extends BaseScreen {
 
 //        add in grid square
         gridSquares = new ArrayList<>();
-        gridSquares.add(new GridSquare(135,135,mainStage));
+        gridSquares.add(new GridSquare(135,135,mainStage,false));
+
 //        add in farmer actor
         farmer = new Farmer(20,20,mainStage);
 
+//        BROKEN
+//        loop through grid squares and activate click functions on each one
+//
+//        for (GridSquare square : gridSquares) {
+//            square.addListener(
+//                    (Event e) ->
+//                    {
+//                        InputEvent ie = (InputEvent)e;
+//                        if ( ie.getType().equals(InputEvent.Type.touchDown) )
+//                            square.clickFunction();
+//                        return false;
+//                    }
+//            );
+//        }
+
     }
 
+
+
+
+
+
+
+
     public void update(float dt) {
-        if (Gdx.input.isKeyPressed(Input.Keys.C)) {
-            gridSquares.get(0).setTexture("grid_red.png");
+        for (GridSquare square : gridSquares) {
+            if (square.getCollisionSetting()) {
+                farmer.preventOverlap(square);
+            }
         }
+    }
+
+
+
+
+
+
+
+    public boolean keyDown(int keyCode)
+    {
+        if (Gdx.input.isKeyPressed(Input.Keys.C)) {
+            if (!gridSquares.get(0).getCollisionSetting()) {
+                gridSquares.get(0).setTexture("grid_red.png");
+                gridSquares.get(0).setCollisionSetting(true);
+            }
+            else {
+                gridSquares.get(0).setTexture("grid_blank.png");
+                gridSquares.get(0).setCollisionSetting(false);
+            }
+
+        }
+        return false;
     }
 
 }
