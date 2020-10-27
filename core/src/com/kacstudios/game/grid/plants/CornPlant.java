4f4445cad27b76dbab1b51a98a978217b1ef4256
churpita <32397472+churpita@users.noreply.github.com>
2020-10-27 09:55:57 -0400

    updated corn textures, added tractor texture

--- core/src/com/kacstudios/game/grid/plants/CornPlant.java
+++ core/src/com/kacstudios/game/grid/plants/CornPlant.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 public class CornPlant extends Plant {
     public CornPlant(boolean collides) {
         super(collides);
-        setGrowthTextures(new String[]{"TestSeed.png", "grid_blank.png", "grid_red.png", "grid_blue.png", "grid_green.png"});
+        setGrowthTextures(new String[]{"soil.png","corn-1.png", "corn-2.png", "corn-3.png", "corn-4.png"});
         setGrowthTime(20l);
     }
 }
