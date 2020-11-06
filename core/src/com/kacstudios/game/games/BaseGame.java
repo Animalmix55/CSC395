9c238dd91ca5557fd51a031fc002594abe268e93
churpita <32397472+churpita@users.noreply.github.com>
2020-11-06 12:35:55 -0500

    Added logo + menu background, tweaked buttons
    Removed background from buttons
    Changed font coloring on buttons to white
    Swapped out main menu texture (left old one in files for now)
    Adjusted button alignment/spacing
    Added logo

--- core/src/com/kacstudios/game/games/BaseGame.java
+++ core/src/com/kacstudios/game/games/BaseGame.java
@@ -55,8 +55,8 @@ public abstract class BaseGame extends Game
         FreeTypeFontParameter fontParameters = new FreeTypeFontParameter();
         fontParameters.size = 48;
         fontParameters.color = Color.WHITE;
-        fontParameters.borderWidth = 2;
-        fontParameters.borderColor = Color.BLACK;
+//        fontParameters.borderWidth = 2;
+//        fontParameters.borderColor = Color.BLACK;
         fontParameters.borderStraight = true;
         fontParameters.minFilter = TextureFilter.Linear;
         fontParameters.magFilter = TextureFilter.Linear;
@@ -68,11 +68,11 @@ public abstract class BaseGame extends Game
 
         textButtonStyle = new TextButtonStyle();
 
-        Texture   buttonTex   = new Texture( Gdx.files.internal("button.png") );
-        NinePatch buttonPatch = new NinePatch(buttonTex, 24,24,24,24);
-        textButtonStyle.up    = new NinePatchDrawable( buttonPatch );
+//        Texture   buttonTex   = new Texture( Gdx.files.internal("button.png") );
+//        NinePatch buttonPatch = new NinePatch(buttonTex, 24,24,24,24);
+//        textButtonStyle.up    = new NinePatchDrawable( buttonPatch );
         textButtonStyle.font      = customFont;
-        textButtonStyle.fontColor = Color.GRAY;
+        textButtonStyle.fontColor = Color.WHITE;
     }
 
     /**
