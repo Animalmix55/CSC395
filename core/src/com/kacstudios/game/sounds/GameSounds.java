192bd30cedeb480d903095b7b27ee168da28244c
Cory Cherven <chervenc@my.canisius.edu>
2020-11-29 21:42:46 -0500

    Added sound effects. Tweaked tractor mounting issue.

---
+++ core/src/com/kacstudios/game/sounds/GameSounds.java
@@ -0,0 +1,12 @@
+package com.kacstudios.game.sounds;
+
+import com.badlogic.gdx.Audio;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.AudioDevice;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
+public class GameSounds {
+    public Music gameMusic = Gdx.audio.newMusic("")
+    public Sound tractorSound = Audio
+}
